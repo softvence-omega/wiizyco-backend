7b352fdfe37210206866206647ba7117fd797ef2
akashsaha02 <asaha8313@gmail.com>
2025-08-18 17:12:44 +0600

    role updated for all the user types ion messages

--- src/modules/messages/messages.routes.ts
+++ src/modules/messages/messages.routes.ts
@@ -9,39 +9,39 @@ const router = express.Router();
 
 router.post(
   '/send',
-  auth(userRole.user, userRole.admin),
+  auth(userRole.user, userRole.admin, userRole.reviver, userRole.founder),
   upload.single('file'),
   messagesController.sendMessage,
 );
 
 router.get(
   '/:otherUserId',
-  auth(userRole.user, userRole.admin),
+  auth(userRole.user, userRole.admin, userRole.reviver, userRole.founder),
   messagesController.getConversation,
 );
 
 // NEW
 router.get(
   '/unread/count',
-  auth(userRole.user, userRole.admin),
+  auth(userRole.user, userRole.admin, userRole.reviver, userRole.founder),
   messagesController.getUnreadCounts,
 );
 
 router.post(
   '/read/:otherUserId',
-  auth(userRole.user, userRole.admin),
+  auth(userRole.user, userRole.admin, userRole.reviver, userRole.founder),
   messagesController.markConversationRead,
 );
 
 router.post(
   '/:id/delete',
-  auth(userRole.user, userRole.admin),
+  auth(userRole.user, userRole.admin, userRole.reviver, userRole.founder),
   messagesController.deleteForMe,
 );
 
 router.post(
   '/:id/retract',
-  auth(userRole.user, userRole.admin),
+  auth(userRole.user, userRole.admin, userRole.reviver, userRole.founder),
   messagesController.retractForEveryone,
 );
 
